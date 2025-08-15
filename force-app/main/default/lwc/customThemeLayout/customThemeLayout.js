9f643db8392e1a83be795ba289e4a703d4af1089
Jeferson Tavares da Silva <jeferson.tavare@caixaresidencial.com.br>
2025-08-15 18:37:14 -0300

    create customThemeLayout

---
+++ force-app/main/default/lwc/customThemeLayout/customThemeLayout.js
@@ -0,0 +1,11 @@
+import { LightningElement } from "lwc";
+
+/**
+ * @slot header this is the header slot
+ * @slot footer this is the footer slot
+ * @slot default this is the default slot
+ * DOCS - https://developer.salesforce.com/docs/atlas.en-us.exp_cloud_lwr.meta/exp_cloud_lwr/get_started_layout.htm
+ * A anotação JSDoc contendo '@slot' seguido do nome da região é necessário para que a plataforma saiba quais slots
+ * (ou regiões no Experience Builder) estão expostos para o seu componente de layout de página. Por
+ */
+export default class CustomThemeLayout extends LightningElement {}
